616f68ac3b1370faae3ef9e8aead5de3059f4f4f
hw commit <demaniks@yahoo.com>
2022-11-01 18:54:41 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        //task 1
+        int person = 17;
+        if (person >= 18) {
+            System.out.println("Поздравляем Вы совершеннолетний!");
+        } else {
+            System.out.println("Ваш возраст для совершенолетия еще не наступил");
+        }
     }
 }
