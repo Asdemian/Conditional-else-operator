66b1d9410100777b2fa8d1c3f1fa563a498e994e
hw commit <demaniks@yahoo.com>
2022-11-01 19:03:52 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,24 @@ public class Main {
         } else if (person >= 24) {
             System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
+        //task 3
+        int carCapacity = 102;
+        int seating = 60;
+        int standingPlaces = carCapacity - seating;
+        int numberOfOccupiedSeats = 60;
+        int numberOfOccupiedStandingPlaces = 42;
+        if (seating > numberOfOccupiedSeats) {
+            System.out.println("В вагоне есть еще " +(seating - numberOfOccupiedSeats)+ " сидячих места.");
+        } else  {
+            System.out.println( "В вагоне нет сидячих мест");
+        }
+        if (standingPlaces > numberOfOccupiedStandingPlaces) {
+            System.out.println("В вагоне есть еще " +(standingPlaces - numberOfOccupiedStandingPlaces)+ " стоячих места.");
+        } else {
+            System.out.println("В вагоне нет стоячих мест");
+        }
+        if (numberOfOccupiedSeats >= seating && numberOfOccupiedStandingPlaces >= standingPlaces) {
+            System.out.println("Вагон уже полностью забит.");
+        }
     }
 }
